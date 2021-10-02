a6762c6be87c19f90f113dab1657a8755823c73d
Jordan <Jordan@MSI>
2021-10-02 09:29:10 -0500

    Add project files.

---
+++ NascarQuiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NascarQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
