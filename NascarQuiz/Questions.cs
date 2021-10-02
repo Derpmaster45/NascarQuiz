8280e48fd62a5dc123d06c3df90bfa4b086629c6
derpmaster45 <jmsteltz04@gmail.com>
2021-10-02 10:19:35 -0500

    Inital Classes are incomplete

---
+++ NascarQuiz/Questions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NascarQuiz
+{
+    class Questions
+    {
+        // use the bool to check answers
+        // round 1 will be ten questions
+        string[] round1Questions = new string[10];
+        Random rand;
+    }
+    
+}
